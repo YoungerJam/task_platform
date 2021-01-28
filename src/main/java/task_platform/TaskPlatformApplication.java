cfcfc10739b3504bfb6b30655b005318be9a4f66
YoungerJam <771007760@qq.com>
2021-01-29 01:28:56 +0800

    init

---
+++ src/main/java/task_platform/TaskPlatformApplication.java
@@ -0,0 +1,13 @@
+package task_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlatformApplication {
+
+	public static void main(String[] args) {
+		int a = 111;
+		SpringApplication.run(TaskPlatformApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/jam/task_platform/TaskPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jam.task_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
